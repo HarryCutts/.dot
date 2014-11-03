f4d0a9eaffb9fb5d3d49935ce08aa4c1d68f1811
Harry Cutts <eternal.linux@gmail.com>
2014-11-03 22:13:29 +0000

    zsh: add `up` function for changing directory

---
+++ common/up-function.zsh
@@ -0,0 +1,26 @@
+# Changes to the ancestor directory with the given name. For example:
+#
+#	$ pwd
+#	/home/user/foo/bar/baz
+#	$ up foo
+#	$ pwd
+#	/home/user/foo
+#
+# Also outputs the ancestor's path, for use in other commands. For example:
+#
+#	$ pwd
+#	/home/user/foo/bar/baz
+#	$ mv quuz.txt $(up foo)
+#
+function up {
+	pathParts=("${(s|/|)$(pwd)}")  # split pwd by slash
+	target=""
+	for part in $pathParts; do
+		target=$target/$part
+		if [[ $part == $1 ]]; then
+			echo $target
+			cd $target
+			break
+		fi
+	done
+}
